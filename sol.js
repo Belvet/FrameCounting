4c146f76707650b5cca05df3d4f8857497019ef5
Martin Migueles <martin.migueles1996@gmail.com>
2023-06-30 18:32:56 -0300

    New interface

--- sol.js
+++ sol.js
@@ -19,19 +19,21 @@ const ky = [
 ];
 
 const getPossibleMoves = function(player0, player1, attackName) {
+// Genero variable con el personaje en cuestion
   const attackMoves = player0;
-
+// Genero una variable con el ataque que uso
   const attackMoveUsed = attackMoves.find(move => move.name === attackName);
+// Genero una variable con el valor de "fdOnBlock" del ataque que uso
   const blockValue = attackMoveUsed.fdOnBlock;
-
+// Mapea todo "attackMoves", y crea una copia identica, pero con el startup de cada ataque cambiado, con respecto al "blockValue" del ataque elegido
   const adjustedStartUp = attackMoves.map(move => {
     return { ...move, startUp: move.startUp - blockValue };
   });
-
+// Revisa el valor "startUp" de cada objeto dentro del array "adjustedStartUp" y trae solo al mas bajo de todos
   const lowestStartUp = Math.min(...adjustedStartUp.map(move => move.startUp));
-
+// Filtra "player1" comparandolo a "lowestStartUp", y genera un nuevo array de objetos, que solo trae aquellos que su valor de "startUp" sea menor que el valor de "lowestStartUp"
   const availableMoves = player1.filter(move => move.startUp <= lowestStartUp);
-
+// Trae un array con los nombres de los ataques que habia en el array de objetos "avaliableMoves"
   const availableMoveNames = availableMoves.map(move => move.name);
 
   document.getElementById("result").textContent = `Available moves for use are: ${availableMoveNames}`;
@@ -42,7 +44,7 @@ const getPossibleMoves = function(player0, player1, attackName) {
 
 
 
-function updateDropdown3Options() {
+function updateDropdownOptions() {
   var dropdown1Value = document.getElementById("dropdown1").value;
   var dropdown2Value = document.getElementById("dropdown2").value;
   var dropdown3 = document.getElementById("dropdown3");
@@ -53,10 +55,18 @@ function updateDropdown3Options() {
     // Add options based on selected values in dropdown1 and dropdown2
     if (dropdown1Value === "sol") {
       // If "Sol Badguy" is selected in both dropdown1 and dropdown2
+      dropdown3.add(new Option("5P", "5P"));
+      dropdown3.add(new Option("c.S", "c.S"));
+      dropdown3.add(new Option("f.S", "f.S"));
+      dropdown3.add(new Option("5H", "5H"));
       dropdown3.add(new Option("S Volcanic Viper", "S Volcanic Viper"));
       dropdown3.add(new Option("Fafnir", "Fafnir"));
     } else if (dropdown1Value === "ky") {
       // If "Ky Kiske" is selected in both dropdown1 and dropdown2
+      dropdown3.add(new Option("5P", "5P"));
+      dropdown3.add(new Option("c.S", "c.S"));
+      dropdown3.add(new Option("f.S", "f.S"));
+      dropdown3.add(new Option("5H", "5H"));
       dropdown3.add(new Option("Foudre Arc", "Foudre Arc"));
       dropdown3.add(new Option("Vapor Thrust", "Vapor Thrust"));
     } else {
@@ -70,7 +80,7 @@ function updateDropdown3Options() {
 
 
 
-function getPossibleMoves1() {
+function displayPossibleMoves() {
   var dropdown1Value = document.getElementById("dropdown1").value;
   var dropdown2Value = document.getElementById("dropdown2").value;
   var dropdown3Value = document.getElementById("dropdown3").value;
@@ -105,3 +115,19 @@ if (dropdown2Value === "sol") {
 
 
   
+
+// Elegir enemigo a travez de la imagen
+
+function enemyImage(enemy) {
+  var dropdown1 = document.getElementById("dropdown1");
+  dropdown1.value = enemy;
+  updateDropdownOptions();
+}
+
+// Elegir enemigo a travez de la imagen
+
+function youImage(you) {
+  var dropdown2 = document.getElementById("dropdown2");
+  dropdown2.value = you;
+  updateDropdownOptions();
+}
